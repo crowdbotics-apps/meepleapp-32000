dbdc970aef4b728f9b8b7460209b5d0a0acc2be0
Crowdbotics <team@crowdbotics.com>
2021-11-15 18:01:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "meepleapp_32000",
+  url: "https://meepleapp_32000.botics.co",
+  api: "https://meepleapp_32000.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
